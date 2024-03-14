8203127593705a25145869fa115c4a3dac7b1db1
Sepriani <163222499+Sepriani@users.noreply.github.com>
2024-03-14 12:42:21 +0700

    Add files via upload

---
+++ adp1.py
@@ -0,0 +1,7 @@
+print("Menghitung Luas Dan Volume")
+r = int(input("Masukkan Jari-Jari"))
+phi = 22/7
+Luas = 4*phi*r*r
+Volume = 4/3*(phi*r*r*r)
+print("Luas Bola Adalah    = ", Luas)
+print ("Volume Bola Adalah = ", Volume)
