28db9d15e5e026582b0ecc6f6370f9366325202c
Sepriani <163222499+Sepriani@users.noreply.github.com>
2024-05-30 15:25:32 +0700

    Add files via upload

---
+++ tugas M 9.py
@@ -0,0 +1,30 @@
+import cprint
+import termcolor
+def draw_cake():
+    for i in range(10):
+        for j in range(10):
+            if (i + j) % 2 == 0:
+                cprint.cprint(f"{i}{j}", "red", "on_white")
+            else:
+                cprint.cprint(f"{i}{j}", "green", "on_white")
+        for i in range(10):
+            cprint.cprint(f"{i}", "yellow", "on_white")
+def draw_cake():
+    for i in range(10):
+        for j in range(10):
+            if (i + j) % 2 == 0:
+                cprint.cprint(f"{i}{j}", "red", "on_white")
+            else:
+                cprint.cprint(f"{i}{j}", "green", "on_white")
+        for i in range(10):
+            cprint.cprint(f"{i}", "yellow", "on_white")
+def draw_cake():
+    for i in range(10):
+        for j in range(10):
+            if (i + j) % 2 == 0:
+                cprint.cprint(f"{i}{j}", "red", "on_white")
+            else:
+                cprint.cprint(f"{i}{j}", "green", "on_white")
+        for i in range(10):
+            cprint.cprint(f"{i}", "yellow", "on_white")
+draw_cake()
